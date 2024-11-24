59781f75d2be0b5362966219d790989918071515
FortiBrine <info@fortibrine.cc>
2024-11-24 16:34:09 +0200

    add delete option to frontend part

--- frontend/api/relationApi.ts
+++ frontend/api/relationApi.ts
@@ -15,3 +15,14 @@ export async function getAllRelations(token: string): Promise<RelationDto[]> {
     })
     return await res.json()
 }
+
+export async function deleteRelation(username: string, token: string): Promise<void> {
+    const res = await fetch("https://learning-app-1ll5.onrender.com/api/relation/remove?username=" + username, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token,
+        }
+    })
+}
