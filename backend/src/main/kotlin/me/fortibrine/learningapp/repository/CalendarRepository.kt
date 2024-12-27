21a8c8b24d1341e405019b9a34bde6f06b7458e1
FortiBrine <FortiBrine@users.noreply.github.com>
2024-12-27 23:46:11 +0200

    make simple query for calendar

--- backend/src/main/kotlin/me/fortibrine/learningapp/repository/CalendarRepository.kt
+++ backend/src/main/kotlin/me/fortibrine/learningapp/repository/CalendarRepository.kt
@@ -3,6 +3,8 @@ package me.fortibrine.learningapp.repository
 import me.fortibrine.learningapp.model.Calendar
 import me.fortibrine.learningapp.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.sql.Timestamp
 
@@ -10,5 +12,10 @@ import java.sql.Timestamp
 interface CalendarRepository: JpaRepository<Calendar, Long> {
     fun findByUser(user: User): List<Calendar>
     fun findByTarget(user: User): List<Calendar>
-    fun findByUserAndFromTimeAfterOrderByFromTime(user: User, fromTimeAfter: Timestamp): MutableList<Calendar>
+
+    @Query("SELECT c FROM calendars c WHERE c.user = :user AND c.fromTime > :fromTime ORDER BY c.fromTime")
+    fun findByUser(
+        @Param("user") user: User,
+        @Param("fromTime") from: Timestamp
+    ): MutableList<Calendar>
 }
