1ea94d1c5061b0aabd127f8c9d4ee12ebd3ef5d6
FortiBrine <FortiBrine@users.noreply.github.com>
2025-04-04 18:15:29 +0300

    Add mappings params to Calendar

--- backend/src/main/kotlin/me/fortibrine/learningapp/mapper/CalendarMapper.kt
+++ backend/src/main/kotlin/me/fortibrine/learningapp/mapper/CalendarMapper.kt
@@ -9,6 +9,9 @@ import org.mapstruct.Mapping
 interface CalendarMapper {
 
     @Mapping(source = "target.name", target = "name")
+    @Mapping(source = "target.username", target = "username")
+    @Mapping(source = "fromTime", target = "from")
+    @Mapping(source = "toTime", target = "to")
     fun toDto(calendar: Calendar): CalendarDto
 
 }
