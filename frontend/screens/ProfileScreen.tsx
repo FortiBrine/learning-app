7884aed56618031ac97a71550add07837ee5185d
FortiBrine <info@fortibrine.cc>
2025-04-05 01:34:21 +0300

    Add simple chat screen (only ui)

--- frontend/screens/ProfileScreen.tsx
+++ frontend/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet, View} from "react-native";
+import {Image, ScrollView, StyleSheet, View} from "react-native";
 import {useNavigation, useRoute} from "@react-navigation/native";
 import {ProfileScreenNavigationProp, ProfileScreenRouteProp} from "../navigation/Navigator";
 import {Button, Chip, Text} from "react-native-paper";
@@ -14,55 +14,67 @@ const ProfileScreen = () => {
     const [t, i18n] = useTranslation();
 
     return (
-        <View style={styles.container}>
-            <Text variant="titleLarge" style={{
-                fontWeight: "bold",
-                textAlign: "center"
-            }}>{route.params.person.name}</Text>
-            <Image style={styles.image} source={{
-                uri: 'https://cdn-icons-png.flaticon.com/128/3024/3024605.png'
-            }} />
+        <ScrollView>
+            <View style={styles.container}>
+                <Text variant="titleLarge" style={{
+                    fontWeight: "bold",
+                    textAlign: "center"
+                }}>{route.params.person.name}</Text>
+                <Image style={styles.image} source={{
+                    uri: 'https://cdn-icons-png.flaticon.com/128/3024/3024605.png'
+                }} />
 
-            <View style={styles.subjectList}>
-                { route.params.person.subjects.map((value, index) => (
-                    <Chip key={index} mode="outlined">
-                        {t(value)}
-                    </Chip>
-                ))}
-            </View>
+                <View style={styles.subjectList}>
+                    { route.params.person.subjects.map((value, index) => (
+                        <Chip key={index} mode="outlined">
+                            {t(value)}
+                        </Chip>
+                    ))}
+                </View>
 
-            <Button mode="outlined" onPress={() => {}}>
-                <Text variant="titleLarge">
-                    {t("get-into-schedule")}
-                </Text>
-            </Button>
-
-            <Button mode="outlined" onPress={() => {}}>
-                <Text variant="titleLarge">
-                    {t("schedule")}
-                </Text>
-            </Button>
-
-            <Button mode="outlined" icon="cloud-upload-outline" onPress={() => {}}>
-                <Text variant="titleLarge">
-                    {t("upload")}
-                </Text>
-            </Button>
-
-            { route.params.addButton && (
-                <Button mode="outlined" onPress={async () => {
-                    await addRelation(route.params.person.username);
-
-                    navigation.navigate("Home");
-                }}>
+                <Button mode="outlined" onPress={() => {}}>
                     <Text variant="titleLarge">
-                        {t("add")}
+                        {t("get-into-schedule")}
                     </Text>
                 </Button>
-            )}
 
-            <StatusBar style="auto" />
-        </View>
+                <Button mode="outlined" onPress={() => {}}>
+                    <Text variant="titleLarge">
+                        {t("schedule")}
+                    </Text>
+                </Button>
+
+                <Button mode="outlined" icon="cloud-upload-outline" onPress={() => {}}>
+                    <Text variant="titleLarge">
+                        {t("upload")}
+                    </Text>
+                </Button>
+
+                { route.params.addButton ? (
+                    <Button mode="outlined" onPress={async () => {
+                        await addRelation(route.params.person.username);
+
+                        navigation.navigate("Home");
+                    }}>
+                        <Text variant="titleLarge">
+                            {t("add")}
+                        </Text>
+                    </Button>
+                ) : (
+                    <Button mode="outlined" onPress={async () => {
+                        navigation.navigate("Chat", {
+                            person: route.params.person
+                        });
+                    }}>
+                        <Text variant="titleLarge">
+                            {t("chat")}
+                        </Text>
+                    </Button>
+                )}
+
+            </View>
+        </ScrollView>
+
     );
 };
 
