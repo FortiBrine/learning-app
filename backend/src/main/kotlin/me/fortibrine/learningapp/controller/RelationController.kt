1a4757fa9106dc297a9891234a4df67d6456ffc1
FortiBrine <FortiBrine@users.noreply.github.com>
2025-04-14 22:01:28 +0300

    go back to global /api

--- backend/src/main/kotlin/me/fortibrine/learningapp/controller/RelationController.kt
+++ backend/src/main/kotlin/me/fortibrine/learningapp/controller/RelationController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/relations")
+@RequestMapping("/api/relations")
 class RelationController (
     private val userRepository: UserRepository,
     private val relationRepository: RelationRepository,
