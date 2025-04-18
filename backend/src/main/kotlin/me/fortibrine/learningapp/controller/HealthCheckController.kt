fdf84862eb1a4f35a57a167c4160bd41b36517c9
FortiBrine <FortiBrine@users.noreply.github.com>
2025-04-18 12:08:53 +0300

    add health check

---
+++ backend/src/main/kotlin/me/fortibrine/learningapp/controller/HealthCheckController.kt
@@ -0,0 +1,12 @@
+package me.fortibrine.learningapp.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+
+    @GetMapping("/health")
+    fun health(): String = "OK"
+
+}
