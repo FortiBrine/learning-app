df7b6259120e823c131310d9772e828f2c3b9a2a
FortiBrine <info@fortibrine.cc>
2025-05-03 20:05:25 +0300

    New error format

--- backend/src/main/kotlin/me/fortibrine/learningapp/exception/handler/GlobalExceptionHandler.kt
+++ backend/src/main/kotlin/me/fortibrine/learningapp/exception/handler/GlobalExceptionHandler.kt
@@ -1,29 +1,68 @@
 package me.fortibrine.learningapp.exception.handler
 
+import jakarta.servlet.http.HttpServletRequest
+import me.fortibrine.learningapp.dto.exception.ErrorDetail
 import me.fortibrine.learningapp.dto.exception.ErrorResponse
-import me.fortibrine.learningapp.dto.exception.ValidationErrorResponse
 import me.fortibrine.learningapp.exception.ValidationError
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import java.lang.Exception
 
 @ControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler(ValidationError::class)
-    fun handleValidationError(exception: ValidationError): ResponseEntity<ValidationErrorResponse> {
+    fun handleValidationError(
+        exception: ValidationError,
+        request: HttpServletRequest,
+    ): ResponseEntity<ErrorResponse> {
         return ResponseEntity.badRequest()
-            .body(ValidationErrorResponse(
-                errors = exception.errors,
-                message = "VALIDATION_ERROR"
+            .body(ErrorResponse(
+                error = ErrorDetail(
+                    type = "VALIDATION_ERROR",
+                    message = "Validation error",
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    path = request.requestURI,
+                    data = exception.errors
+                )
             ))
     }
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
-    fun handleException(exception: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
+    fun handleNotReadableException(
+        exception: HttpMessageNotReadableException,
+        request: HttpServletRequest,
+    ): ResponseEntity<ErrorResponse> {
         return ResponseEntity.badRequest()
-            .body(ErrorResponse("INVALID_BODY"))
+            .body(ErrorResponse(
+                error = ErrorDetail(
+                    type = "INVALID_BODY",
+                    message = "Invalid body",
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    path = request.requestURI,
+                    data = null
+                )
+            ))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handlerOtherException(
+        exception: Exception,
+        request: HttpServletRequest,
+    ): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.badRequest()
+            .body(ErrorResponse(
+                error = ErrorDetail(
+                    type = "INTERNAL_SERVER_ERROR",
+                    message = "Internal Server Error",
+                    status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    path = request.requestURI,
+                    data = null
+                )
+            ))
     }
 
 }
