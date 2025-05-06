1c5a8f34a05598517516aa773239c385fa8d9026
FortiBrine <FortiBrine@users.noreply.github.com>
2025-05-06 20:43:29 +0300

    moved /api/subjects to /api/profile/setting. Add roles

--- backend/src/main/kotlin/me/fortibrine/learningapp/controller/ProfileController.kt
+++ backend/src/main/kotlin/me/fortibrine/learningapp/controller/ProfileController.kt
@@ -1,19 +1,20 @@
 package me.fortibrine.learningapp.controller
 
+import me.fortibrine.learningapp.dto.user.SettingUserDto
 import me.fortibrine.learningapp.dto.user.UserDto
 import me.fortibrine.learningapp.mapper.UserMapper
 import me.fortibrine.learningapp.model.User
 import me.fortibrine.learningapp.repository.RatingRepository
+import me.fortibrine.learningapp.repository.UserRepository
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/profile")
 class ProfileController(
     private val userMapper: UserMapper,
-    private val ratingRepository: RatingRepository
+    private val ratingRepository: RatingRepository,
+    private val userRepository: UserRepository
 ) {
 
     @GetMapping
@@ -22,5 +23,19 @@ class ProfileController(
     ): UserDto =
         userMapper.toDto(principal, ratingRepository.findAverageRatingByTarget(principal) ?: 0.0)
 
+    @PostMapping("/setting")
+    fun settingProfile (
+        @RequestBody payload: SettingUserDto,
+
+        @AuthenticationPrincipal principal: User,
+    ) {
+        if (payload.name != null) principal.name = payload.name
+        if (payload.email != null) principal.email = payload.email
+        if (payload.subjects != null)
+            principal.subjects = payload.subjects.toMutableSet()
+        if (payload.role != null) principal.role = payload.role
+
+        userRepository.save(principal)
+    }
 
 }
--- backend/src/main/kotlin/me/fortibrine/learningapp/dto/user/UserDto.kt
+++ backend/src/main/kotlin/me/fortibrine/learningapp/dto/user/UserDto.kt
@@ -1,9 +1,12 @@
 package me.fortibrine.learningapp.dto.user
 
+import me.fortibrine.learningapp.model.Role
+
 data class UserDto (
     val name: String,
     val username: String,
     val email: String,
     val subjects: List<String>,
-    val rating: Double
+    val rating: Double,
+    val role: Role
 )
